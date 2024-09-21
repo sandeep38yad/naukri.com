e4395e65cd1dd13da72192e60d741272c35b6677
Sandeep <sandeep38yad@gmail.com>
2024-09-21 15:57:57 +0530

    bat file updated

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
 REM Use the full path to python.exe and pytest module
-"C:\Users\sande\AppData\Local\Programs\Python\Python311\python.exe" -m pytest -v -s --html=.\Reports\report.html .\testCases\test_login.py .\testCases\test_profile.py .\testCases\test_jobsearch.py --browser chrome
+pytest -v -s --html=.\Reports\report.html .\testCases\test_login.py .\testCases\test_profile.py .\testCases\test_jobsearch.py --browser firefox
 REM Pause to keep the terminal window open after execution
 pause
